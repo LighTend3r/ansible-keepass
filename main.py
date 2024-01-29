490287a895cadea9df8d58c94c0a26e47bb6c8b3
LighTender - Dev <87587438+LighTend3r@users.noreply.github.com>
2024-01-29 15:55:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ version_added: "1.0.0"
 description: Write and read in keepass (.kdbs). Based on https://pypi.org/project/pykeepass/
 
 options:
-    file:
+    db_file:
         description: Path of the database (.kdbs).
         required: true
         type: str
@@ -122,14 +122,14 @@ EXAMPLES = r'''
 # Create database
 - name: Create database
   keepass:
-    file: '/tmp/test.kdbx'
+    db_file: '/tmp/test.kdbx'
     db_password: 'password'
     state: 'create'
 
 # Create group
 - name: Create group
   keepass:
-    file: '/tmp/test.kdbx'
+    db_file: '/tmp/test.kdbx'
     db_password: 'password'
     which: 'group'
     state: 'touch'
@@ -138,7 +138,7 @@ EXAMPLES = r'''
 # Create entrie
 - name: Create entrie
   keepass:
-    file: '/tmp/test.kdbx'
+    db_file: '/tmp/test.kdbx'
     db_password: 'password'
     which: 'entrie'
     state: 'touch'
@@ -158,33 +158,60 @@ entries:
     type: List
     returned: always
 find:
-    description: List of any find.
-    type: List|str
+    description: List of any find/modified.
+    type: List
     returned: sometimes
 '''
 
 from ansible.module_utils.basic import AnsibleModule
 
 from pykeepass import PyKeePass, create_database
+from collections.abc import Iterable
 import re
 
+def est_iterable(objet):
+    return isinstance(objet, Iterable)
+
+def build_keyword(module_params, keys):
+    return {k: module_params[k] for k in keys if module_params[k] is not None}
+
 def action_groups(module, kp, result):
+    if module.params['group_name'] is None:
+            module.fail_json(msg='Error, group_name is required', **result)
+
     if module.params['state'] == 'touch':
-        if module.params['group_name'] not in result['groups']:
-            kp.add_group(kp.root_group, module.params['group_name'], notes=module.params['notes'], uuid=module.params['uuid'])
+        if module.params['group_name'] not in [group.name for group in kp.groups]:
+
+            keys = ['notes', 'uuid']
+            keyword = build_keyword(module.params, keys)
+
+            group = kp.add_group(kp.root_group, module.params['group_name'], **keyword)
+
+            result['find'] = [group]
             result['changed'] = True
     elif module.params['state'] == 'find':
-        result['find'] = kp.find_groups(name=module.params['group_name'], first=module.params['first'], regex=module.params['regex'], notes=module.params['notes'], uuid=module.params['uuid'], recursive=module.params['recursive'], path=module.params['path'])
+
+        keys = ['notes', 'uuid', 'path']
+        keyword = build_keyword(module.params, keys)
+
+        groups = kp.find_groups(name=module.params['group_name'], first=module.params['first'], regex=module.params['regex'], recursive=module.params['recursive'], **keyword)
+
         if module.params['first'] == True:
-            if result['find'] is None:
+            if groups is None:
                 module.fail_json(msg='Error, group doesn\'t exist', **result)
+            result['find'] = [groups]
         else:
-            if len(result['find']) == 0:
+            if len(groups) == 0:
                 module.fail_json(msg='Error, group doesn\'t exist', **result)
+            result['find'] = groups
     elif module.params['state'] == 'delete':
-        if module.params['group_name'] in result['groups']:
+        if module.params['group_name'] in [group.name for group in kp.groups]:
             try:
-                kp.delete_group(kp.find_groups(name=module.params['group_name'], first=True, regex=module.params['regex'], notes=module.params['notes'], uuid=module.params['uuid'], recursive=module.params['recursive'], path=module.params['path']))
+
+                keys = ['notes', 'uuid', 'path']
+                keyword = build_keyword(module.params, keys)
+
+                kp.delete_group(kp.find_groups(name=module.params['group_name'], first=True, regex=module.params['regex'], recursive=module.params['recursive'], **keyword))
                 result['changed'] = True
             except Exception as e:
                 module.fail_json(msg='Error on delete group', **result)
@@ -192,24 +219,44 @@ def action_groups(module, kp, result):
         module.fail_json(msg='Error, state does\'t exist for groups', **result)
 
 def action_entries(module, kp, result):
+
     if module.params['state'] == 'touch':
 
-        # Get the group
-        if module.params['group_name'] not in result['groups']:
-            if module.params['always_create']:
-                group = kp.add_group(kp.root_group, module.params['group_name'])
-                result['changed'] = True
-            else:
-                module.fail_json(msg='Error, group doesn\'t exist', **result)
+        find = None
+
+        if module.params['group_name'] is None:
+            group = kp.root_group
         else:
-            group = kp.find_groups(name=module.params['group_name'], first=True, regex=module.params['regex'], recursive=module.params['recursive'])
+            groups = kp.find_groups(name=module.params['group_name'])
+            if module.params['group_name'] not in [group.name for group in groups]:
+                if module.params['always_create']:
+                    group = kp.add_group(kp.root_group, module.params['group_name'])
+                    result['changed'] = True
+                    find = [group]
+                else:
+                    module.fail_json(msg='Error, group doesn\'t exist', **result)
+            else:
+                group = kp.find_groups(name=module.params['group_name'], first=True, regex=module.params['regex'], recursive=module.params['recursive'])
+
+        if group is None:
+            group = kp.root_group
 
         if module.params['username'] is None or module.params['password'] is None:
             module.fail_json(msg='Error, username and password are required', **result)
 
-        if module.params['entry_title'] not in [i.title for i in group]: # If the entrie doesn't exist
-            kp.add_entry(group, module.params['entry_title'], module.params['username'], module.params['password'], url=module.params['url'], notes=module.params['notes'], tags=module.params['tags'], uuid=module.params['uuid'])
+        if module.params['entry_title'] is None:
+            module.fail_json(msg='Error, entry_title is required', **result)
+
+        keys = ['url', 'notes', 'uuid', 'tags']
+        keyword = build_keyword(module.params, keys)
+
+        if module.params['entry_title'] not in [entry.title for entry in group.entries]:
+            entrie = kp.add_entry(group, module.params['entry_title'], module.params['username'], module.params['password'], **keyword)
             result['changed'] = True
+            if find is None:
+                result['find'] = [entrie]
+            else:
+                result['find'].append(entrie)
 
 
     elif module.params['state'] == 'find':
@@ -220,13 +267,19 @@ def action_entries(module, kp, result):
                 module.fail_json(msg='Error, group doesn\'t exist', **result)
 
         if module.params['entry_title'] is not None:
-            result['find'] = kp.find_entries(title=module.params['entry_title'], group=group, first=module.params['first'], regex=module.params['regex'], url=module.params['url'], notes=module.params['notes'], tags=module.params['tags'], uuid=module.params['uuid'], recursive=module.params['recursive'], path=module.params['path'])
-            if module.params['first'] == True and result['find'] is None:
-                module.fail_json(msg='Error, entrie doesn\'t exist', **result)
-            else:
-                if len(result['find']) == 0:
-                    module.fail_json(msg='Error, entrie doesn\'t exist', **result)
 
+            keys = ['url', 'notes', 'tags', 'uuid', 'path']
+            keyword = build_keyword(module.params, keys)
+
+            entries = kp.find_entries(title=module.params['entry_title'], group=group, first=module.params['first'], regex=module.params['regex'], recursive=module.params['recursive'], **keyword)
+            if module.params['first'] == True:
+                if entries is None:
+                    module.fail_json(msg='Error, entrie doesn\'t exist', **result)
+                result['find'] = [entries]
+            else:
+                if len(entries) == 0:
+                    module.fail_json(msg='Error, entrie doesn\'t exist', **result)
+                result['find'] = entries
 
 
 
@@ -239,7 +292,10 @@ def action_entries(module, kp, result):
 
         if module.params['entry_title'] is not None:
             try:
-                kp.delete_entry(kp.find_entries(title=module.params['entry_title'], group=group, first=True, regex=module.params['regex'], url=module.params['url'], notes=module.params['notes'], tags=module.params['tags'], uuid=module.params['uuid'], recursive=module.params['recursive'], path=module.params['path']))
+                keys = ['url', 'notes', 'tags', 'uuid', 'path']
+                keyword = build_keyword(module.params, keys)
+
+                kp.delete_entry(kp.find_entries(title=module.params['entry_title'], group=group, first=True, regex=module.params['regex'], recursive=module.params['recursive'], **keyword))
                 result['changed'] = True
             except Exception as e:
                 module.fail_json(msg='Error on delete entrie', **result)
@@ -251,6 +307,8 @@ def listed(module, kp, result):
         result['groups'] = [
         {
             'name': group.name,
+            'path': group.path,
+            'notes': group.notes,
             'entries': [entry.title for entry in group.entries]
         } for group in kp.groups]
     else:
@@ -266,28 +324,22 @@ def listed(module, kp, result):
             'notes': entry.notes,
             'tags': entry.tags,
             'expires': entry.expires,
-            'expires_at': entry.expires_at,
-            'created': entry.created,
-            'updated': entry.updated,
-            'accessed': entry.accessed,
-            'binary_desc': entry.binary_desc,
-            'binary': entry.binary,
             'history': entry.history,
             'icon': entry.icon,
-            'uuid': entry.uuid,
-            'parent_group': entry.parent_group,
+            'uuid': str(entry.uuid),
+            'parent_group': entry.parentgroup.path,
         } for entry in kp.entries]
 
 
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        file=dict(type='str', required=True),
-        db_password=dict(type='str', required=True),
+        db_file=dict(type='str', required=True),
+        db_password=dict(type='str', required=True, no_log=True),
         which=dict(type='str', required=False),
         state=dict(type='str', required=False),
         username=dict(type='str', required=False),
-        password=dict(type='str', required=False),
+        password=dict(type='str', required=False, no_log=True),
         first=dict(type='bool', required=False, default=True),
         group_name=dict(type='str', required=False),
         always_create=dict(type='bool', required=False, default=False),
@@ -331,24 +383,25 @@ def run_module():
     # state with no modifications
 
     # Update the list
-    listed(module, kp, result)
+
 
 
     if module.params['state'] == 'create':
         try: # Try to open the database
-            kp = PyKeePass(module.params['file'], password=module.params['db_password'])
+            kp = PyKeePass(module.params['db_file'], password=module.params['db_password'])
         except Exception as e:
-            kp = create_database(module.params['file'], password=module.params['db_password'], keyfile=None, transformed_key=None)
+            kp = create_database(module.params['db_file'], password=module.params['db_password'], keyfile=None, transformed_key=None)
             result['changed'] = True
 
         kp.save()
         module.exit_json(**result)
     else:
         try:
-            kp = PyKeePass(module.params['file'], password=module.params['db_password'])
+            kp = PyKeePass(module.params['db_file'], password=module.params['db_password'])
         except Exception as e:
             module.fail_json(msg='Error on open database', **result)
 
+    listed(module, kp, result)
 
     # Target the good action
     if module.params['which'] == 'group':
